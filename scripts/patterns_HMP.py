1e32c2711bfcd2c990fc68ce4dca5a7331e887ff
robertmarsland <robertvsiii@gmail.com>
2019-10-09 15:18:44 -0400

    broke patterns
    figured out how to break each of the patterns to respond to referees
    
    
    Former-commit-id: 8a1bc6b4b86053df5afc55130fdb7c30a4bec23b

--- scripts/patterns_HMP.py
+++ scripts/patterns_HMP.py
@@ -17,11 +17,10 @@ folder= '../data/'
 n_samples = 300
 R0_food = 1000
 
-###############GENERAL SETUP#####################
 mp = {'sampling':'Binary', #Sampling method
     'SA': np.ones(6)*800, #Number of species in each family
     'MA': np.ones(6)*50, #Number of resources of each type
-    'Sgen': 200, #Number of generalist species
+    'Sgen': 20, #Number of generalist species
     'muc': 10, #Mean sum of consumption rates in Gaussian model
     'q': 0.9, #Preference strength (0 for generalist and 1 for specialist)
     'c0':0, #Background consumption rate in binary model
@@ -42,10 +41,11 @@ def dRdt(N,R,params):
 dynamics = [dNdt,dRdt]
 #Construct matrices
 c,D = MakeMatrices(mp)
+#Make dictionaries to hold data
+N = {}
+metadata = {}
 
-###############SET UP EXPERIMENT#####################
-#NOTE: R0_food and food are irrelevant here, because we make a new
-#R0 matrix later on
+## Set up shared parameters
 HMP_protocol = {'R0_food':R0_food, #unperturbed fixed point for supplied food
                 'n_wells':3*n_samples, #Number of independent wells
                 'S':2500, #Number of species per well
@@ -54,8 +54,10 @@ HMP_protocol = {'R0_food':R0_food, #unperturbed fixed point for supplied food
 HMP_protocol.update(mp)
 #Make initial state
 N0,R0 = MakeInitialState(HMP_protocol)
+
+################Two external resources####################
 R0 = np.zeros(np.shape(R0))
-alpha = np.linspace(0,1,n_samples)
+alpha = np.random.rand(n_samples)
 for k in range(3):
     R0[2*k*50,k*n_samples:(k+1)*n_samples] = alpha*R0_food
     R0[(2*k+1)*50,k*n_samples:(k+1)*n_samples] = (1-alpha)*R0_food
@@ -75,31 +77,23 @@ for k in range(len(params)):
     params[k]['D'] = D
     params[k]['m'] = m
 HMP = Community(init_state,dynamics,params)
-HMP.metadata = pd.DataFrame(['Env. 1']*n_samples+['Env. 2']*n_samples+['Env. 3']*n_samples,
+HMP.metadata = pd.DataFrame(['Site 1']*n_samples+['Site 2']*n_samples+['Site 3']*n_samples,
                             index=N0.T.index,columns=['Environment'])
-
-###############GET STEADY STATE AND SAVE#####################
 HMP.SteadyState(plot=False,tol=1e-3,verbose=False)
-with open(folder+'HMP_env_family.dat','wb') as f:
+with open(folder+'HMP_env_two.dat','wb') as f:
     pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
+N['Two resource HMP'] = HMP.N
+metadata['Two resource HMP'] = HMP.metadata
 
-###############REDO WITHOUT FAMILY STRUCTURE#####################
-HMP_protocol.update({'SA': 6*800+200, #Number of species in each family
-    'MA': 6*50, #Number of resources of each type
-    'Sgen': 0, #Number of generalist species
-    'muc': 10, #Mean sum of consumption rates in Gaussian model
-    'q': 0,#Preference strength (0 for generalist and 1 for specialist)
-    'waste_type':0
-})
-c,D = MakeMatrices(HMP_protocol)
-N0,R0 = MakeInitialState(HMP_protocol)
+#############All external resources####################
 R0 = np.zeros(np.shape(R0))
-alpha = np.linspace(0,1,n_samples)
 for k in range(3):
-    R0[2*k*50,k*n_samples:(k+1)*n_samples] = alpha*R0_food
-    R0[(2*k+1)*50,k*n_samples:(k+1)*n_samples] = (1-alpha)*R0_food
+    R0_temp = np.random.rand(mp['MA'][2*k]+mp['MA'][2*k+1],n_samples)
+    R0_temp = (R0_temp/R0_temp.sum(axis=0))*R0_food
+    R0[mp['MA'][:2*k].sum():mp['MA'][:2*(k+1)].sum(),k*n_samples:(k+1)*n_samples] = R0_temp
 R0 = pd.DataFrame(R0,index=D.index,columns=N0.keys())
 init_state=[N0,R0]
+#Make parameter list
 m = 1+0.01*np.random.randn(len(c))
 params=[{'w':1,
         'g':1,
@@ -113,9 +107,14 @@ for k in range(len(params)):
     params[k]['D'] = D
     params[k]['m'] = m
 HMP = Community(init_state,dynamics,params)
-HMP.metadata = pd.DataFrame(['Env. 1']*n_samples+['Env. 2']*n_samples+['Env. 3']*n_samples,
+HMP.metadata = pd.DataFrame(['Site 1']*n_samples+['Site 2']*n_samples+['Site 3']*n_samples,
                             index=N0.T.index,columns=['Environment'])
+HMP.SteadyState(plot=False,tol=1e-3,verbose=False)
+with open(folder+'HMP_env_all.dat','wb') as f:
+    pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
+N['All resource HMP'] = HMP.N
+metadata['All resource HMP'] = HMP.metadata
 
-HMP.SteadyState(plot=False,tol=1e-3)
-with open(folder+'HMP_env.dat','wb') as f:
-                    pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
+for exp in N.keys():
+    N[exp].to_csv(folder+'_'.join(['N']+exp.split(' '))+'.csv')
+    metadata[exp].to_csv(folder+'_'.join(['m']+exp.split(' '))+'.csv')
