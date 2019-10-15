d151370c5b0bf962b70f1503d376efa5d4be130f
robertmarsland <robertvsiii@gmail.com>
2019-10-15 14:29:16 -0400

    updated scripts
    revised HMP script, systematizing file names and adjusting level of stochastic colonization
    
    
    Former-commit-id: 2901e92c19a98c460ada3edb559e568a87bc598b

--- scripts/patterns_HMP.py
+++ scripts/patterns_HMP.py
@@ -20,7 +20,7 @@ R0_food = 1000
 mp = {'sampling':'Binary', #Sampling method
     'SA': np.ones(6)*800, #Number of species in each family
     'MA': np.ones(6)*50, #Number of resources of each type
-    'Sgen': 20, #Number of generalist species
+    'Sgen': 200, #Number of generalist species
     'muc': 10, #Mean sum of consumption rates in Gaussian model
     'q': 0.9, #Preference strength (0 for generalist and 1 for specialist)
     'c0':0, #Background consumption rate in binary model
@@ -48,7 +48,7 @@ metadata = {}
 ## Set up shared parameters
 HMP_protocol = {'R0_food':R0_food, #unperturbed fixed point for supplied food
                 'n_wells':3*n_samples, #Number of independent wells
-                'S':2500, #Number of species per well
+                'S':4900, #Number of species per well
                 'food':0 #index of food source
                 }
 HMP_protocol.update(mp)
@@ -56,6 +56,7 @@ HMP_protocol.update(mp)
 N0,R0 = MakeInitialState(HMP_protocol)
 
 ################Two external resources####################
+exp = 'Two resource HMP'
 R0 = np.zeros(np.shape(R0))
 alpha = np.random.rand(n_samples)
 for k in range(3):
@@ -80,12 +81,13 @@ HMP = Community(init_state,dynamics,params)
 HMP.metadata = pd.DataFrame(['Site 1']*n_samples+['Site 2']*n_samples+['Site 3']*n_samples,
                             index=N0.T.index,columns=['Environment'])
 HMP.SteadyState(plot=False,tol=1e-3,verbose=False)
-with open(folder+'HMP_env_two.dat','wb') as f:
+with open(folder+'_'.join(['comm']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.dat','wb') as f:
     pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
-N['Two resource HMP'] = HMP.N
-metadata['Two resource HMP'] = HMP.metadata
+HMP.N.to_csv(folder+'_'.join(['N']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
+HMP.metadata.to_csv(folder+'_'.join(['m']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
 
 #############All external resources####################
+exp = 'All resource HMP'
 R0 = np.zeros(np.shape(R0))
 for k in range(3):
     R0_temp = np.random.rand(mp['MA'][2*k]+mp['MA'][2*k+1],n_samples)
@@ -110,11 +112,38 @@ HMP = Community(init_state,dynamics,params)
 HMP.metadata = pd.DataFrame(['Site 1']*n_samples+['Site 2']*n_samples+['Site 3']*n_samples,
                             index=N0.T.index,columns=['Environment'])
 HMP.SteadyState(plot=False,tol=1e-3,verbose=False)
-with open(folder+'HMP_env_all.dat','wb') as f:
+with open(folder+'_'.join(['comm']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.dat','wb') as f:
     pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
-N['All resource HMP'] = HMP.N
-metadata['All resource HMP'] = HMP.metadata
+HMP.N.to_csv(folder+'_'.join(['N']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
+HMP.metadata.to_csv(folder+'_'.join(['m']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
 
-for exp in N.keys():
-    N[exp].to_csv(folder+'_'.join(['N']+exp.split(' '))+'.csv')
-    metadata[exp].to_csv(folder+'_'.join(['m']+exp.split(' '))+'.csv')
+#############Random Axis####################
+exp = 'Random Axis HMP'
+R0 = np.zeros(np.shape(R0))
+for k in range(3):
+    R0_temp = BinaryRandomMatrix(mp['MA'].sum(),2,1/6)
+    R0_temp = (R0_temp/R0_temp.sum(axis=0))*R0_food
+    R0[:,k*n_samples:(k+1)*n_samples] = R0_temp[:,0][:,np.newaxis].dot(alpha[np.newaxis,:])+R0_temp[:,1][:,np.newaxis].dot(1-alpha[np.newaxis,:])
+R0 = pd.DataFrame(R0,index=D.index,columns=N0.keys())
+init_state=[N0,R0]
+#Make parameter list
+m = 1+0.01*np.random.randn(len(c))
+params=[{'w':1,
+        'g':1,
+        'l':0.8,
+        'R0':R0.values[:,k],
+        'r':1.,
+        'tau':1
+        } for k in range(len(N0.T))]
+for k in range(len(params)):
+    params[k]['c'] = c
+    params[k]['D'] = D
+    params[k]['m'] = m
+HMP = Community(init_state,dynamics,params)
+HMP.metadata = pd.DataFrame(['Site 1']*n_samples+['Site 2']*n_samples+['Site 3']*n_samples,
+                            index=N0.T.index,columns=['Environment'])
+HMP.SteadyState(plot=False,tol=1e-3,verbose=False)
+with open(folder+'_'.join(['comm']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.dat','wb') as f:
+    pickle.dump([HMP.N,HMP.R,params[0],R0,HMP.metadata],f)
+HMP.N.to_csv(folder+'_'.join(['N']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
+HMP.metadata.to_csv(folder+'_'.join(['m']+exp.split(' '))+'S'+str(HMP_protocol['S'])+'.csv')
